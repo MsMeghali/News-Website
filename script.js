1104d9eb19f93ed1107795121c71947ece44598a
megha <meghasaini095@gmail.com>
2023-08-22 22:23:46 +0530

    Optimized code

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-let div = document.getElementById('outer');
+const outer = document.getElementById('outer');
 
 fetch('./data.json')
     .then((d) => d.json())
     .then((data) => {
         data.articles.forEach(element => {
-            let div1 = document.createElement('div')
-            div1.setAttribute('class', 'div1')
+            const [inner, category, heading, link, standfirst, article, image, hr] = createElements('div', 'span', 'h1', 'a', 'p', 'div', 'img', 'hr')
 
-            let category = document.createElement('span');
-            category.setAttribute('class', 'category')
+            inner.classList.add('inner')
+            heading.classList.add('heading')
+            standfirst.classList.add('standfirst')
+            article.classList.add('article')
+            image.classList.add('image')
 
-            let heading = document.createElement('h1');
-            heading.setAttribute('class', 'heading')
-
-            let link = document.createElement('a');
-            link.setAttribute('href', element.link);
-            link.appendChild(heading);
-
-            let standfirst = document.createElement('p');
-            standfirst.setAttribute('class', 'standfirst');
-
-            let div2 = document.createElement('div');
-            div2.setAttribute('class', 'div2');
-
-            let image = document.createElement('img');
-            image.setAttribute('class', 'image')
             image.setAttribute('height', element.thumbnail.height)
             image.setAttribute('width', element.thumbnail.width)
             image.setAttribute('alt', element.thumbnail.title)
+            image.setAttribute('src', element.thumbnail.src)
+            link.setAttribute('href', element.link)
 
-            category.innerHTML = element.primarySectionRouteName;
-            heading.innerHTML = element.headline;
-            standfirst.innerHTML = element.standfirst;
-            image.setAttribute('src', element.thumbnail.src);
+            category.innerHTML = element.primarySectionRouteName
+            heading.innerHTML = element.headline
+            standfirst.innerHTML = element.standfirst
 
-            div2.appendChild(category);
-            div2.appendChild(link);
-            div2.appendChild(standfirst);
-            div1.appendChild(div2);
-            div1.appendChild(image);
-            div.appendChild(div1);
-
-            let hr = document.createElement('hr');
-            div.appendChild(hr);
+            addChild(link, heading)
+            addChild(article, category, link, standfirst)
+            addChild(inner, article, image)
+            addChild(outer, inner, hr)
         });
+    })
+    .catch(err => {
+        console.log(err);
     });
+
+function createElements(...elements) {
+    const list = []
+    for (let e of elements) {
+        list.push(document.createElement(e))
+    }
+    return list;
+}
+
+function addChild(parent, ...childs) {
+    for (let element of childs) {
+        parent.appendChild(element)
+    }
+}
